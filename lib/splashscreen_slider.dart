9253ff7eadc2c42401d6c0a8655b9e1697e810be
mranhtu <mrtula01@gmail.com>
2019-12-10 23:57:10 +0700

    Fix bug slide

--- lib/splashscreen_slider.dart
+++ lib/splashscreen_slider.dart
@@ -1,27 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
-class SplashScreenSlider extends StatelessWidget {
+class SplashScreenSlider extends StatefulWidget {
+  @override
+  _SplashScreenSliderState createState() => _SplashScreenSliderState();
+}
+
+class _SplashScreenSliderState extends State<SplashScreenSlider> {
+  List<Map<String, dynamic>> listSlide = [
+    {"image": 'icon1.png', "title": "Title"},
+    {"image": 'icon2.png', "title": "Title"},
+    {"image": 'icon3.png',"title": "Title"}
+    ];
+
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: Color(0xff0001FC),
       body:  Swiper(
         itemBuilder: (BuildContext context, int index) {
-          return  Image.asset(
-            'icon1.png',
-            fit: BoxFit.fill,
+          return  buildContent(context,
+            title: listSlide[index]['title'],
+            image: listSlide[index]['image'],
           );
-
         },
-        itemHeight: 260,
-        itemWidth: 360,
-        itemCount: 3,
-        layout: SwiperLayout.STACK,
-        pagination: SwiperPagination(),
-        viewportFraction: 0.8,
+        itemWidth: screenSize.width,
+        itemCount: listSlide.length,
+        pagination: SwiperPagination(
+          margin: EdgeInsets.only(bottom: screenSize.height*0.21)
+        ),
+        viewportFraction: 1.0,
         scale: 0.9,
       )
     );
   }
+
+ Widget buildContent(BuildContext context, {String image, String title}){
+    final screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(
+            height: screenSize.height*0.43,
+            decoration: BoxDecoration(
+              color: Color(0xff0001FC),
+              image: DecorationImage(
+                image: AssetImage(image),
+                fit: BoxFit.cover
+              )
+            ),
+          ),
+          ///todo
+          Positioned(
+            bottom: screenSize.height*0.11,
+            child: Text(title,
+              style: TextStyle(color: Colors.white
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+ }
 }
