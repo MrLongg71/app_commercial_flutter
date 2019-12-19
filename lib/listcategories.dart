4eedafff2e29bb76263a249ce463935da4f7ae34
mranhtu <mrtula01@gmail.com>
2019-12-19 22:51:53 +0700

    Fix bug margin item

--- lib/listcategories.dart
+++ lib/listcategories.dart
@@ -19,122 +19,124 @@ class ListCategories extends StatelessWidget {
           size: 30,
         ),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-
-          padding: EdgeInsets.only(left: 16.0, right: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Text(
-                'Categories',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold),
-              ),
-              Material(
-                elevation: 10,
-                color: Colors.white,
-                child: Container(
-
-                  width: double.infinity,
-                  height: 75,
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Text(
-                      "All",
-                      style: TextStyle(color: Colors.black, fontSize: 18),
-                    ),
-                  ),
-                ),
-              ),
-
-              Material(
-                elevation: 10,
-                color: Colors.white,
-                child: Container(
-                  width: double.infinity,
-                  height: 75,
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: GestureDetector(
-                      onTap: (){
-                        Navigator.of(context).pushReplacementNamed('/listlaptop');
-                      },
+      body: ListView(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 25.0, bottom: 25.0),
+            child: Text(
+              'Categories',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListView.builder(
+              shrinkWrap: true,
+              itemCount: 4,
+              padding: EdgeInsets.only(left: 16, right: 16),
+              itemBuilder: (_, int index){
+                return Container(
+                  margin: EdgeInsets.only(bottom: 10.0),
+                  child: Material(
+                    elevation: 3.0,
+                    color: Colors.white,
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      width: double.infinity,
+                      height: 75,
+                      padding: EdgeInsets.only(left: 25),
                       child: Text(
-                        "Computers",
+                        "All",
                         style: TextStyle(color: Colors.black, fontSize: 18),
                       ),
                     ),
                   ),
-                ),
-              ),
-              Material(
-                elevation: 10,
-                color: Colors.white,
-                child: Container(
-                  width: double.infinity,
-                  height: 75,
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Text(
-                      "Accessories",
-                      style: TextStyle(color: Colors.black, fontSize: 18),
-                    ),
-                  ),
-                ),
-              ),
-              Material(
-                elevation: 10,
-                color: Colors.white,
-                child: Container(
-                  width: double.infinity,
-                  height: 75,
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Text(
-                      "Smartphones",
-                      style: TextStyle(color: Colors.black, fontSize: 18),
-                    ),
-                  ),
-                ),
-              ),
-              Material(
-                elevation: 10,
-                color: Colors.white,
-                child: Container(
-
-                  width: double.infinity,
-                  height: 75,
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Text(
-                      "Smart objects",
-                      style: TextStyle(color: Colors.black, fontSize: 18),
-                    ),
-                  ),
-                ),
-              ),
-              Material(
-                elevation: 10,
-                color: Colors.white,
-                child: Container(
-                  width: double.infinity,
-                  height: 75,
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Text(
-                      "Speakers",
-                      style: TextStyle(color: Colors.black, fontSize: 18),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+                );
+              }
           ),
-        ),
+//              Material(
+//                elevation: 10,
+//                color: Colors.white,
+//                child: Container(
+//                  width: double.infinity,
+//                  height: 75,
+//                  child: Padding(
+//                    padding: EdgeInsets.all(20),
+//                    child: GestureDetector(
+//                      onTap: (){
+//                        Navigator.of(context).pushReplacementNamed('/listlaptop');
+//                      },
+//                      child: Text(
+//                        "Computers",
+//                        style: TextStyle(color: Colors.black, fontSize: 18),
+//                      ),
+//                    ),
+//                  ),
+//                ),
+//              ),
+//              Material(
+//                elevation: 10,
+//                color: Colors.white,
+//                child: Container(
+//                  width: double.infinity,
+//                  height: 75,
+//                  child: Padding(
+//                    padding: EdgeInsets.all(20),
+//                    child: Text(
+//                      "Accessories",
+//                      style: TextStyle(color: Colors.black, fontSize: 18),
+//                    ),
+//                  ),
+//                ),
+//              ),
+//              Material(
+//                elevation: 10,
+//                color: Colors.white,
+//                child: Container(
+//                  width: double.infinity,
+//                  height: 75,
+//                  child: Padding(
+//                    padding: EdgeInsets.all(20),
+//                    child: Text(
+//                      "Smartphones",
+//                      style: TextStyle(color: Colors.black, fontSize: 18),
+//                    ),
+//                  ),
+//                ),
+//              ),
+//              Material(
+//                elevation: 10,
+//                color: Colors.white,
+//                child: Container(
+//
+//                  width: double.infinity,
+//                  height: 75,
+//                  child: Padding(
+//                    padding: EdgeInsets.all(20),
+//                    child: Text(
+//                      "Smart objects",
+//                      style: TextStyle(color: Colors.black, fontSize: 18),
+//                    ),
+//                  ),
+//                ),
+//              ),
+//              Material(
+//                elevation: 10,
+//                color: Colors.white,
+//                child: Container(
+//                  width: double.infinity,
+//                  height: 75,
+//                  child: Padding(
+//                    padding: EdgeInsets.all(20),
+//                    child: Text(
+//                      "Speakers",
+//                      style: TextStyle(color: Colors.black, fontSize: 18),
+//                    ),
+//                  ),
+//                ),
+//              ),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Color(0xffEFF5FB),
